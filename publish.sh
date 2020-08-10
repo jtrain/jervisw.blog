d6c694ff34d3596aaaf4d3b495eb87bc6ef365b2
Jervis Whitley <jtrain@users.noreply.github.com>
2020-08-10 13:23:36 +1000

    gh-pages -> hugo-pages

--- publish.sh
+++ publish.sh
@@ -12,8 +12,8 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+echo "Checking out hugo-pages branch into public"
+git worktree add -B hugo-pages public origin/hugo-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -21,8 +21,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating hugo-pages branch"
+cd public && git add --all && git commit -m "Publishing to hugo-pages (publish.sh)"
 
 echo "Pushing to github"
 git push --all
